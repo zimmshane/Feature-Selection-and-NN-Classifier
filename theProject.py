05e3361e2ad4a3e54bb6b734676bc1ad45c1c857
Lindsay Adams <lindsayadams2552@gmail.com>
2024-11-13 18:35:23 -0800

    Added backward selection

--- theProject.py
+++ theProject.py
@@ -40,9 +40,40 @@ class FeatureSearch:
             
         return currentFeatures
     
+    def backwardSelection(self)->list:
+        n = len(self.featureList)
+        parentAccuracy = -math.inf
+        currentFeatures=set(self.featureList)
+        depth = 0
+        while depth < n:
+            bestChildAccuracy = (-math.inf,0) #(eval,index of that item)
+            
+            for i in range(n):
+                if self.featureList[i] not in currentFeatures: continue
+                currentFeatures.remove(self.featureList[i]) 
+                eval = self.evaluate()
+                print(f"Evaluated {currentFeatures} at {eval} ")
+                if eval > bestChildAccuracy[0]:
+                    bestChildAccuracy=(eval,i)
+                currentFeatures.add(self.featureList[i]) 
+                
+            if bestChildAccuracy[0] < parentAccuracy: # No better options dont add -> exit
+                print(f"No children with better accuracy, returning!")
+                break
+            
+            currentFeatures.remove(self.featureList[bestChildAccuracy[1]]) #Removing the best child from feature list
+            print(f"adding feature {self.featureList[bestChildAccuracy[1]]}! new set: {currentFeatures}, New accuracy {bestChildAccuracy[0]}")
+            parentAccuracy = bestChildAccuracy[0]
+            depth += 1
+            
+        return currentFeatures
+
+        
+    
 class Printer:
     pass
     
 if __name__ == "__main__":
     feet = FeatureSearch()
     print(f"best set: {feet.forwardSelection()}")
+    print(f"best set: {feet.backwardSelection()}")
