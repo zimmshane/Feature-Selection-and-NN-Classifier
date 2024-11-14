b3436e905eb55ff5fbe6297aa92459d61ec88c92
Shane Zimmerman <zimmshane@gmail.com>
2024-11-14 06:09:25 -0800

    Merge Conflict Resolution

--- theProject.py
+++ theProject.py
@@ -2,39 +2,43 @@
 import math
 import random
 
+MAX_FEATURE_COUNT = 20
+
 class FeatureSearch:
     featureList = []
     
     def __init__(self,featureList = range(5)):
         self.featureList = featureList
     
-    def evaluate(self):
-        return random.randint(1,100)
+    def evaluate(self): 
+        return random.randint(1,100) #stubbed
 
     def forwardSelection(self)->list:
         n = len(self.featureList)
         parentAccuracy = -math.inf
-        currentFeatures=set()
+        currentFeatures = set()
         depth = 0
+        print(Printer.searchStartForward)
         while depth < n:
-            bestChildAccuracy = (-math.inf,0)
+            bestChildAccuracy = (-math.inf,None) # (EVAL_SCORE, FEATURE_INDEX_TO_ADD)
             
             for i in range(n):
                 if self.featureList[i] in currentFeatures: continue
                 currentFeatures.add(self.featureList[i]) 
                 eval = self.evaluate()
-                print(f"Evaluated {currentFeatures} at {eval} ")
+                print(f"{currentFeatures} Evaluated at {eval}")
                 if eval > bestChildAccuracy[0]:
-                    bestChildAccuracy=(eval,i)
+                    bestChildAccuracy = (eval,i)
                     
                 currentFeatures.remove(self.featureList[i]) #backtrack
                 
             if bestChildAccuracy[0] < parentAccuracy: # No better options dont add -> exit
-                print(f"No children with better accuracy, returning!")
+                print(Printer.searchQuit)
                 break
             
-            currentFeatures.add(self.featureList[bestChildAccuracy[1]]) #Add back best branch
-            print(f"adding feature {self.featureList[bestChildAccuracy[1]]}! new set: {currentFeatures}, New accuracy {bestChildAccuracy[0]}")
+            currentFeatures.add(self.featureList[bestChildAccuracy[1]]) # Add back best branch
+            print(f"\nAdding Feature {self.featureList[bestChildAccuracy[1]]}" )
+            print(f"New Feature Set: {currentFeatures}: Accuracy {bestChildAccuracy[0]}\n")
             parentAccuracy = bestChildAccuracy[0]
             depth += 1
             
@@ -71,9 +75,21 @@ class FeatureSearch:
         
     
 class Printer:
-    pass
+    mainWelcome : str = "Welcome to SZIMM011 and LADAM020's Project 2!"
+    searchStartForward : str = "Starting Forward Selection Search... "
+    searchQuit : str = "All Children Result in Lower Accuracy, Terminating Search..."
     
+    @staticmethod
+    def featureCountPrompt() -> int:
+        prompt = "Enter the number of features"
+        valIn = input(prompt)
+        if valIn.isnumeric() and int(valIn) < MAX_FEATURE_COUNT:
+            return int(valIn)
+        return 5
+            
+        
 if __name__ == "__main__":
+    print(Printer.mainWelcome)
     feet = FeatureSearch()
     print(f"best set: {feet.forwardSelection()}")
     print(f"best set: {feet.backwardSelection()}")
